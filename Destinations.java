af96069a0b559bd8b4ef4e0e4d1ada3cdb0b1d1c
Nevno48 <nevno48@gmail.com>
2019-04-16 12:24:24 -0400

    Add files via upload

---
+++ Destinations.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Destinations - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Destinations
+{
+    LINCON_CENTER, CENTRAL_PARK, TIMES_SQUARE, MIDTOWN_WEST, UNITED_NATIONS, EMPIRE_STATE_BUILDING, CHELSEA, GRAMERCY_PARK, GREENWICH_VILLAGE, SOHO, CHINATOWN, LOWER_EAST_SIDE, WALL_STREET, BROOKLYN 
+}
