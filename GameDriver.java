e4d27e5d428dd713086b6eb25bef2205fcf9d5b9
Greg MacGown <31485651+gmacg1@users.noreply.github.com>
2019-04-18 14:29:25 -0400

    Basic draw transportation card method
    Includes comments for where more direct implementation needed after other classes are complete

--- GameDriver.java
+++ GameDriver.java
@@ -10,4 +10,25 @@ public class GameDriver
     //Starter file
     
     //Driver class which runs the game, synthesizing the parts into a whole
+    boolean init = false;
+    
+    /**
+     * Method which draws transportation cards
+     */
+    public void drawTrans() {
+        //full implementation to be done later
+
+        boolean blind = true; //drawing a card from the pile is a blind draw 
+        for (int i = 0; i < 2; i++){
+            if (!init) return; //draw two blind cards
+            else {
+                //implement mouse listening for coordinates to click on cards
+                //on board or the trans deck
+                if (!blind) {
+                    //if the card is a rainbow card
+                    break;
+                }
+            }
+        }
+    }
 }
