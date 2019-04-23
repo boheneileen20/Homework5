6a295d23d9144d2042099ad7574ad7b516a4ea8b
Eileen Bohen <43003611+boheneileen20@users.noreply.github.com>
2019-04-23 17:02:26 -0400

    added opening screen and player count selection, added transport cards

--- TicketToRide.java
+++ TicketToRide.java
@@ -3,27 +3,41 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.awt.event.*;
 import java.util.*;
+
 /**
  * Write a description of class TicketToRide here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * //15 taxis per user
+ * //44 transport cards
+ * //18 destination cards
+ * //if a player has less than 3 taxis, other players get one more turn, and game is over
+ * //score 
+ * @author Eileen Bohen
+ * @version Spring 2019
  */
 public class TicketToRide extends JPanel implements MouseListener
 {
     private int width;
     private int height;
+    private Image coverImage;
     private Image boardImage;
     private Image transportationCardBack;
     private Toolkit toolkit;
     private Rectangle transportationCardPile = new Rectangle(100,100,100,100);
     private ArrayList<DestinationCard> destCards = new ArrayList<>();
+    private ArrayList<TransportationCard> transCards = new ArrayList<>();
+    private ArrayList<TransportationCard> transCardsUpright = new ArrayList<>();
+    private ArrayList<TransportationCard> displayTransCards = new ArrayList<>();
+    private Rectangle playGame;
+    private boolean inGame;
+    private int numPlayers;
+    //private ArrayList<Player> players = new ArrayList<>();
     /**
      * Constructor for objects of class TextTwist
      */
     public TicketToRide() {
+        inGame = false;
         //sets size of window
-        setPreferredSize(new Dimension(800, 800));
+        setPreferredSize(new Dimension(1000, 800));
         toolkit = Toolkit.getDefaultToolkit();
         //sets graphics of window
         boardImage = toolkit.getImage("fwdboardandtransport/game_board.jpg");
@@ -35,8 +49,12 @@ public class TicketToRide extends JPanel implements MouseListener
 
         //instantiate instance variable images
         transportationCardBack = toolkit.getImage("fwdboardandtransport/transportation_card_back.jpg");
-        
-        //read in transportation card images and save them in an arraylist
+
+        //get board cover image
+        coverImage = toolkit.getImage("fwdpieces/box_cover.jpg");
+        playGame = new Rectangle(400, 375, 200,50);
+
+        //read in destination card images and save them in an arraylist
         DestinationCard centralChelsea5 = new DestinationCard(5, "CENTRAL_PARK", "CHELSEA", toolkit.getImage("fwdboardandtransport/central_chelsea_5.jpg"));
         DestinationCard centralChina8 = new DestinationCard(8, "CENTRAL_PARK", "CHINATOWN", toolkit.getImage("fwdboardandtransport/central_china_8.jpg"));
         DestinationCard centralGramercy4 = new DestinationCard(4, "CENTRAL_PARK", "GRAMERCY_PARK", toolkit.getImage("fwdboardandtransport/central_gramercy_4.jpg"));
@@ -54,9 +72,66 @@ public class TicketToRide extends JPanel implements MouseListener
         DestinationCard timesBrooklyn8 = new DestinationCard(8, "TIMES_SQUARE", "BROOKLYN", toolkit.getImage("fwdboardandtransport/times_brooklyn_8.jpg"));     
         DestinationCard timesEast4 = new DestinationCard(4, "TIMES_SQUARE", "EAST_VILLAGE", toolkit.getImage("fwdboardandtransport/times_east_4.jpg")); 
         DestinationCard timesSoho6 = new DestinationCard(6, "TIMES_SQUARE", "SOHO", toolkit.getImage("fwdboardandtransport/times_soho_6.jpg")); 
-        DestinationCard UNWall8 = new DestinationCard(8, "UNITED_NATIONS", "WALL_STREET", toolkit.getImage("fwdboardandtransport/UN_wall_8.jpg"));                                                           
-    }
+        DestinationCard UNWall8 = new DestinationCard(8, "UNITED_NATIONS", "WALL_STREET", toolkit.getImage("fwdboardandtransport/UN_wall_8.jpg"));  
 
+        destCards.add(centralChelsea5);
+        destCards.add(centralChina8);
+        destCards.add(centralGramercy4);
+        destCards.add(centralMidtown3);
+        destCards.add(chelseaBrooklyn8);
+        destCards.add(chelseaWall6);
+        destCards.add(chinaGramercy4);
+        destCards.add(eastSoho4);
+        destCards.add(empireBrooklyn7);
+        destCards.add(empireGreen3);
+        destCards.add(lincolnEmpire3);
+        destCards.add(lincolnGreen6);
+        destCards.add(lowerEastWall2);
+        destCards.add(midWestUN3);
+        destCards.add(timesBrooklyn8);
+        destCards.add(timesEast4);
+        destCards.add(timesSoho6);
+        destCards.add(UNWall8);
+
+        //read in transportation card images and save them in an arraylist
+        TransportationCard blue1 = new TransportationCard("BLUE", toolkit.getImage("fwdpieces/blue_1.jpg"));
+        TransportationCard blue2 = new TransportationCard("BLUE", toolkit.getImage("fwdpieces/blue_2.jpg"));
+        TransportationCard gray1 = new TransportationCard("GRAY", toolkit.getImage("fwdpieces/gray_1.jpg"));
+        TransportationCard gray2 = new TransportationCard("GRAY", toolkit.getImage("fwdpieces/gray_2.jpg"));
+        TransportationCard green1 = new TransportationCard("GREEN", toolkit.getImage("fwdpieces/green_1.jpg"));
+        TransportationCard green2 = new TransportationCard("GREEN", toolkit.getImage("fwdpieces/green_2.jpg"));
+        TransportationCard orange1 = new TransportationCard("ORANGE", toolkit.getImage("fwdpieces/orange_1.jpg"));
+        TransportationCard orange2 = new TransportationCard("ORANGE", toolkit.getImage("fwdpieces/orange_2.jpg"));
+        TransportationCard pink1 = new TransportationCard("ORANGE", toolkit.getImage("fwdpieces/orange_1.jpg"));
+        TransportationCard pink2 = new TransportationCard("ORANGE", toolkit.getImage("fwdpieces/orange_2.jpg"));
+        TransportationCard rainbow1 = new TransportationCard("RAINBOW", toolkit.getImage("fwdpieces/rainbow_1.jpg"));
+        TransportationCard rainbow2 = new TransportationCard("RAINBOW", toolkit.getImage("fwdpieces/rainbow_2.jpg"));
+        TransportationCard red1 = new TransportationCard("RED", toolkit.getImage("fwdpieces/red_1.jpg"));
+        TransportationCard red2 = new TransportationCard("RED", toolkit.getImage("fwdpieces/red_2.jpg"));
+
+        transCards.add(blue1);
+        transCards.add(gray1);
+        transCards.add(green1);
+        transCards.add(orange1);
+        transCards.add(pink1);
+        transCards.add(rainbow1);
+        transCards.add(red1);
+
+        transCardsUpright.add(blue2);
+        transCardsUpright.add(gray2);
+        transCardsUpright.add(green2);
+        transCardsUpright.add(orange2);
+        transCardsUpright.add(pink2);
+        transCardsUpright.add(rainbow2);
+        transCardsUpright.add(red2);
+
+        //shuffle transportation cards and pick four to display
+        Collections.shuffle(transCards);
+        for(int i = 0; i<4; i++){
+            displayTransCards.add(transCards.get(i));
+        }
+
+    }
     /**
      * PaintComponent method for JPanel.
      *
@@ -66,21 +141,39 @@ public class TicketToRide extends JPanel implements MouseListener
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        //draw board such that there is white space on the right side
-        g.drawImage(boardImage, 0, 0, 550, 800, 0,0, 3337, 5048,this);
-        //add pile of transportation cards
-        g.setColor(Color.YELLOW);
-        g.fillRect(550, 0, 250, 150);
-        g.drawImage(transportationCardBack, 550, 0, 800, 150, 0,0, 769, 504, this);
-        
-        
-        //add pile of destination cards
-        
-        //add taxis
-        
-        //add player scores
-        
-        
+        if(inGame == false){
+            //display cover
+            g.drawImage(coverImage, 0,0, width, height, this);
+            //add "PLAY GAME" button
+            ((Graphics2D)g).fill(playGame);
+            //add "PLAY GAME" text
+            g.setColor(Color.YELLOW);
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 32));
+            g.drawString("PLAY GAME", 410, 408);
+
+        }
+
+        else{
+            //draw board such that there is white space on both sides
+            g.drawImage(boardImage, 200, 0, 750, 800, 0,0, 3337, 5048,this);
+            //add pile of transportation cards
+            g.setColor(Color.YELLOW);
+            g.fillRect(750, 0, 550, 150);
+            g.drawImage(transportationCardBack, 750, 0, 1000, 150, 0,0, 769, 504, this);
+
+            //draw 4 displayed transport cards
+            g.drawImage(displayTransCards.get(0).getPicture(), 750, 150, 1000, 300, 0,0, 769, 504, this);
+            g.drawImage(displayTransCards.get(1).getPicture(), 750, 300, 1000, 450, 0,0, 769, 504, this);
+            g.drawImage(displayTransCards.get(2).getPicture(), 750, 450, 1000, 600, 0,0, 769, 504, this);
+            g.drawImage(displayTransCards.get(3).getPicture(), 750, 600, 1000, 750, 0,0, 769, 504, this);
+
+            //add pile of destination cards
+
+            //add taxis
+
+            //add player scores
+
+        }
     }
 
     /**
@@ -149,6 +242,23 @@ public class TicketToRide extends JPanel implements MouseListener
      * 
      */
     public void mouseClicked( MouseEvent e ) {
+        //handle clicking on opening screen
+        if(inGame == false){
+            if(playGame.contains(e.getPoint())){
+                JOptionPane jPane = new JOptionPane("");
+                numPlayers = Integer.parseInt(jPane.showInputDialog("Enter number of players (1,2,3, or 4)"));
+                if(numPlayers >0 && numPlayers<5){
+                    inGame = true;
+                    repaint();
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "You have entered an invalid number of players.");
+                }
+            }
+        }
+        else{
+
+        }
 
     }
 
@@ -158,7 +268,7 @@ public class TicketToRide extends JPanel implements MouseListener
      * @param args command line arguments. 
      */
     public static void main(String[] args) {
-        
+
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -167,4 +277,9 @@ public class TicketToRide extends JPanel implements MouseListener
                 }
             });
     }
+
+    //beginning: pick 3 dest keep 1-3
+    //turn: pick 3 more dest cards, or take 2 transport cards, unless you get rainbow face up
+    //always 4 transport cards face up, replace every time you take
+    //claim route: pay thatamount of transport cards
 }
