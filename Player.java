c6a43dd18c09cc7b420ab0a9cafdb25b49ecc9ce
Eileen Bohen <43003611+boheneileen20@users.noreply.github.com>
2019-04-26 16:24:53 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -15,20 +16,11 @@ public class Player
     //the number of taxis that the player has (15 to begin with)
     public int taxis;
     //the array of transportation cards that the user has
-    private int gray = 0;
-    private int blue = 0;
-    private int orange = 0;
-    private int red = 0;
-    private int green = 0;
-    private int pink = 0;
-    private int rainbow = 0;
     public ArrayList <TransportationCard> transHand;
     //the array of destination cards that the user has
     public ArrayList <DestinationCard> destHand;
     //the array of destinations that the player has successfully claimed
     public ArrayList <Destinations> claimed;
-    //score keeper
-    public int score = 0;
 
     /**
      * Constructor for player objects. Instantiates name and age to the user inputs, taxis to 15, their hands to empty, 
@@ -48,27 +40,20 @@ public class Player
     }
 
     /**
-     * Getter method used to check number of taxis/score
-     * @return int number of taxis player has/current score
+     * Getter method used to check number of taxis
+     * @return int number of taxis player has
      */
 
-    public int getTaxis(){return taxis;}
+    public int getTaxis(){
+        return taxis;
+    }
     
-    public int getScore(){return score;}
     
     /**
      * Methods used to add drawn cards to the player's hand
      * @param TransportationCard c to add to the player's hand
      */
     public void addToTransHand(TransportationCard c) {
-        String color = c.getColor();
-        if (color.equals("GRAY"))gray++;
-        if (color.equals("ORANGE"))orange++;
-        if (color.equals("GREEN"))green++;
-        if (color.equals("BLUE"))blue++;
-        if (color.equals("PINK"))pink++;
-        if (color.equals("RED"))red++;
-        if (color.equals("RAINBOW"))rainbow++;
         transHand.add(c);
     }
     
@@ -80,3 +65,29 @@ public class Player
     public void addToDestHand(DestinationCard c) {
         destHand.add(c);
     }
+
+    public ArrayList<TransportationCard> getTransHand(){
+        return transHand;
+    }
+
+    public void printTransHand(){
+        System.out.print(name + "'s transportation hand: ");
+        for(TransportationCard t: transHand){
+            System.out.print(t.getColor() + " ");
+        }
+    }
+
+    public void printDestHand(){
+        System.out.print(name + "'s destination hand: ");
+        for(DestinationCard t: destHand){
+            System.out.print(t.toString() + " ");
+        }
+
+
+    }
+    public String getName(){
+        return name;
+    }
+    
+    
+}
