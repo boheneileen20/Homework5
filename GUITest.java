e4f1a5be688df5b8f261718542685f62409d52cd
Eileen Bohen <43003611+boheneileen20@users.noreply.github.com>
2019-04-27 16:31:14 -0400

    gui now supports drawing trans cards blindly

---
+++ GUITest.java
@@ -0,0 +1,537 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+/**
+ * Class to create and access panels needed for interface
+ */
+public class GUITest extends JPanel implements MouseListener {
+    /* Create and set up the window.*/
+    private JFrame frame =  new  JFrame("Ticket To Ride NY");
+
+    /* width and height of display*/
+    private int width;
+    private int height;
+    /* Images of the board,box cover,and back of the transport card*/
+    private Image coverImage;
+    private Image destinationCardBack;
+    /* Toolkit used for grabbing Images*/
+    private static Toolkit toolkit = Toolkit.getDefaultToolkit();;
+    private TTRDriver driver;
+    //private static Image boardImage;
+
+    //center board panel
+     JPanel game;
+    //left side panel
+    JPanel left;
+    //top panel
+     JPanel top;
+    //bottom panel
+     JPanel bottom;
+    //right panel
+     JPanel right;
+
+     boolean turnOver;
+
+    public GUITest(){
+
+        //set up players first
+        int numPlayers = Integer.parseInt(JOptionPane.showInputDialog("How many players?"));
+        driver = new TTRDriver(numPlayers);
+
+
+        //call makePlayer for each player
+        for(int i = 1; i<=numPlayers; i++){
+            String name = JOptionPane.showInputDialog("Enter player " + i + "'s name.");
+            int age = Integer.parseInt(JOptionPane.showInputDialog("Enter player " + i + "'s age."));
+            driver.makePlayer(name, age);
+        }
+
+
+        //deal initial trans cards
+        driver.dealInitialTransCards();
+
+
+        /* sets size of window*/
+        frame.setPreferredSize( new  Dimension(1000, 800));
+
+        frame.setBackground(Color.WHITE);
+        /* adds functionality of mouse*/
+        addMouseListener(this);
+
+        //deal dest cards
+        for(Player p: driver.getPlayers()){
+            ArrayList<DestinationCard> choices = driver.drawTwoDest();
+            String playerChoice = JOptionPane.showInputDialog(p.getName() + ", you have drawn these cards: "+ choices.get(0).toString() + " and " +
+                    choices.get(1) + " \n" +
+                    "Enter \"both\" to take both, \"1\" for the first card, and \"2\" for the second");
+            driver.dealInitialDestCardsGUI(playerChoice, choices, p);
+        }
+        JFrame frame = new JFrame("GUITest");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+    }
+
+    /**
+     * Creates the window and GUI with a label
+     */
+    public void createAndShowGUI()
+    {
+
+        //background panel
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.white);
+        panel.setPreferredSize(new Dimension(1000, 800));
+        panel.setLayout(new BorderLayout());
+
+        //center board panel
+        game = boardPanel();
+        //left side panel
+        left = leftPanel();
+        //top panel
+        top = topPanel();
+        JButton startTurnButton = new JButton("Start Turn");
+        top.add(startTurnButton, BorderLayout.EAST);
+
+        startTurnButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                //startTurn = true;
+                int turnChoice = Integer.parseInt(JOptionPane.showInputDialog(frame, "enter 1 to draw trans cards, 2 for dest, 3 to claim route"));
+
+                //handle drawing trans cards
+                if(turnChoice == 1){
+                    String tCardChoice = (JOptionPane.showInputDialog(frame, "Enter \"blind\" to draw from the top of the transportation deck" +
+                        "Enter \"face\" to draw a face card. You may only draw once if you take a taxi card."));
+                    if(tCardChoice.equals("blind")){
+                        TransportationCard t = driver.getTransDeck().get(0);
+                        JOptionPane.showMessageDialog(frame, "You have drawn a " + t.getColor() + " card.");
+                        driver.getPlayers().get(driver.getPlayerTurn()).addToTransHand(t);
+                        driver.removeFromTransDeck(0);
+
+                    }
+
+                    //face draw
+                    else if(tCardChoice.equals("face")){
+
+                    }
+
+                    //repeat for second card
+                    tCardChoice = (JOptionPane.showInputDialog(frame, "Enter \"blind\" to draw from the top of the transportation deck" +
+                            "Enter \"face\" to draw a face card. You may not draw a rainbow taxi card."));
+
+                    if(tCardChoice.equals("blind")){
+                        TransportationCard t = driver.getTransDeck().get(0);
+                        JOptionPane.showMessageDialog(frame, "You have drawn a " + t.getColor() + " card. Your turn is now over.");
+                        driver.getPlayers().get(driver.getPlayerTurn()).addToTransHand(t);
+                        driver.removeFromTransDeck(0);
+
+                    }
+
+                    //face draw
+                    else if(tCardChoice.equals("face")){
+
+                    }
+
+                }
+
+
+                turnOver = true;
+
+                driver.nextPlayersTurn();
+                Player p = driver.getPlayers().get(driver.getPlayerTurn());
+                JOptionPane.showMessageDialog(frame, p.getName() + " is up next");
+
+                //background panel
+                JPanel panel2 = new JPanel();
+                panel2.setBackground(Color.white);
+                panel2.setPreferredSize(new Dimension(1000, 800));
+                panel2.setLayout(new BorderLayout());
+
+
+                //center board panel
+                game = boardPanel();
+                //left side panel
+                left = leftPanel();
+
+                //top panel
+                top = topPanel();
+                //bottom panel
+                bottom = bottomPanel();
+                //right panel
+                right = rightPanel();
+
+                panel2.add(game, BorderLayout.CENTER);
+                panel2.add(right, BorderLayout.EAST);
+                panel2.add(bottom, BorderLayout.SOUTH);
+                panel2.add(top, BorderLayout.NORTH);
+                panel2.add(left, BorderLayout.WEST);
+
+                frame.getContentPane().add(panel2);
+                frame.validate();
+                createAndShowGUI();
+            }
+        });
+        //bottom panel
+        bottom = bottomPanel();
+        //right panel
+        right = rightPanel();
+
+        panel.add(game, BorderLayout.CENTER);
+        panel.add(right, BorderLayout.EAST);
+        panel.add(bottom, BorderLayout.SOUTH);
+        panel.add(top, BorderLayout.NORTH);
+        panel.add(left, BorderLayout.WEST);
+
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+
+
+    //creates right panel, hold face up transport cards and blind draw pile.
+    public JPanel rightPanel(){
+        JPanel p = new JPanel();
+        p.setSize(new Dimension(50, 800));
+        p.setBackground(Color.WHITE);
+        p.setLayout(new GridLayout(12,1));
+
+        //number label
+        JLabel firstLabel = new JLabel("1");
+        firstLabel.setFont(new Font("Calibri",1, 15));
+        firstLabel.setMaximumSize(new Dimension(100, 10));
+
+        Image tCard = driver.getDisplayTransCards().get(0).getPicture();
+        tCard = tCard.getScaledInstance(200,125, 0);
+        JLabel picLabel = new JLabel(new ImageIcon(tCard));
+
+        //number label
+        JLabel secondLabel = new JLabel("2");
+        secondLabel.setFont(new Font("Calibri",1, 15));
+        secondLabel.setMaximumSize(new Dimension(100, 10));
+
+        Image tCard2= driver.getDisplayTransCards().get(1).getPicture();
+        tCard2 = tCard2.getScaledInstance(200,125, 0);
+        JLabel picLabel2 = new JLabel(new ImageIcon(tCard2));
+
+        //number label
+        JLabel thirdLabel = new JLabel("3");
+        thirdLabel.setFont(new Font("Calibri",1, 15));
+        thirdLabel.setMaximumSize(new Dimension(100, 10));
+
+        Image tCard3= driver.getDisplayTransCards().get(2).getPicture();
+        tCard3 = tCard3.getScaledInstance(200,125, 0);
+        JLabel picLabel3 = new JLabel(new ImageIcon(tCard3));
+
+        //number label
+        JLabel fourthLabel = new JLabel("4");
+        fourthLabel.setFont(new Font("Calibri",1, 15));
+        fourthLabel.setMaximumSize(new Dimension(100, 10));
+
+        Image tCard4= driver.getDisplayTransCards().get(3).getPicture();
+        tCard4 = tCard4.getScaledInstance(200,125, 0);
+        JLabel picLabel4 = new JLabel(new ImageIcon(tCard4));
+
+        //number label
+        JLabel fifthLabel = new JLabel("5");
+        fifthLabel.setFont(new Font("Calibri",1, 15));
+        fifthLabel.setMaximumSize(new Dimension(100, 10));
+
+        Image tCard5= driver.getDisplayTransCards().get(4).getPicture();
+        tCard5 = tCard5.getScaledInstance(200,125, 0);
+        JLabel picLabel5 = new JLabel(new ImageIcon(tCard5));
+
+
+        Image blindPile = toolkit.getImage("C:/Users/patri/Documents/eileen/ttrrestored/src/fwdpieces/transportation_back.jpg");
+        blindPile = blindPile.getScaledInstance(200,125, 0);
+        JLabel blindPileLabel = new JLabel(new ImageIcon(blindPile));
+
+        p.add(picLabel);
+        p.add(firstLabel);
+        p.add(picLabel2);
+        p.add(secondLabel);
+        p.add(picLabel3);
+        p.add(thirdLabel);
+        p.add(picLabel4);
+        p.add(fourthLabel);
+        p.add(picLabel5);
+        p.add(fifthLabel);
+        p.add(blindPileLabel);
+
+        return p;
+    }
+    //creates bottom panel
+    public JPanel bottomPanel(){
+        JPanel p = new JPanel();
+        p.setSize(new Dimension(1000, 100));
+        p.setBackground(Color.RED);
+        JPanel panel = new JPanel();
+        JLabel jLabel = new JLabel("Something might go here");
+        jLabel.setFont(new Font("Calibri",1, 15));
+        panel.add(jLabel);
+        panel.setBorder(new LineBorder(Color.BLACK));
+
+        p.add(panel);
+        return p;
+    }
+
+    //creates top panel
+    public JPanel topPanel(){
+        JPanel p = new JPanel();
+        p.setSize(new Dimension(1000, 100));
+        p.setBackground(Color.RED);
+        JPanel panel = new JPanel();
+        JLabel jLabel = new JLabel("It is " + driver.getPlayers().get(driver.getPlayerTurn()).getName() + "'s turn.");
+        jLabel.setFont(new Font("Calibri",1, 15));
+        panel.add(jLabel, BorderLayout.WEST);
+        panel.setBorder(new LineBorder(Color.BLACK));
+
+
+
+        p.add(panel);
+        return p;
+    }
+    /*creates left side panel for player info,
+            1)num taxis
+            2)transportation hand
+            3)destination hand
+        also pile of destination cards
+     */
+    public JPanel leftPanel(){
+        JPanel p = new JPanel();
+        p.setSize(new Dimension(200, 800));
+        p.setBackground(Color.BLUE);
+        p.setLayout(new GridLayout(5,1));
+
+        JPanel transCardHandPanel = new JPanel();
+        transCardHandPanel.setSize(new Dimension(200, 500));
+        transCardHandPanel.setBackground(Color.WHITE);
+        transCardHandPanel.setLayout(new GridLayout(8,2));
+
+        //label for name
+        JLabel nameLabel = new JLabel(driver.getPlayers().get(driver.getPlayerTurn()).getName() + "'s");
+        nameLabel.setFont(new Font("Calibri",1, 15));
+
+        Image blue = driver.getUprightTrans().get(0).getPicture();
+        blue = blue.getScaledInstance(100,100, 0);
+        JLabel bluePile = new JLabel(new ImageIcon(blue));
+
+        //blank space
+        JLabel blankLabel = new JLabel("cards");
+        blankLabel.setFont(new Font("Calibri",1, 15));
+
+        //label for num blue cards
+        JLabel blueLabel = new JLabel("Blues: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("blue"));
+        blueLabel.setFont(new Font("Calibri",1, 15));
+
+
+        Image gray = driver.getUprightTrans().get(1).getPicture();
+        gray = gray.getScaledInstance(100,100, 0);
+        JLabel grayPile = new JLabel(new ImageIcon(gray));
+
+        //label for num gray cards
+        JLabel grayLabel = new JLabel("Grays: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("gray"));
+        grayLabel.setFont(new Font("Calibri",1, 15));
+
+        Image green = driver.getUprightTrans().get(2).getPicture();
+        green = green.getScaledInstance(100,100, 0);
+        JLabel greenPile = new JLabel(new ImageIcon(green));
+
+        //label for num green cards
+        JLabel greenLabel = new JLabel("Greens: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("green"));
+        greenLabel.setFont(new Font("Calibri",1, 15));
+
+        Image orange = driver.getUprightTrans().get(3).getPicture();
+        orange = orange.getScaledInstance(100,100, 0);
+        JLabel orangePile = new JLabel(new ImageIcon(orange));
+
+        //label for num orange cards
+        JLabel orangeLabel = new JLabel("Oranges: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("orange"));
+        orangeLabel.setFont(new Font("Calibri",1, 15));
+
+        Image pink = driver.getUprightTrans().get(4).getPicture();
+        pink = pink.getScaledInstance(100,100, 0);
+        JLabel pinkPile = new JLabel(new ImageIcon(pink));
+
+        //label for num pink cards
+        JLabel pinkLabel = new JLabel("Pinks: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("pink"));
+        pinkLabel.setFont(new Font("Calibri",1, 15));
+
+        Image red = driver.getUprightTrans().get(5).getPicture();
+        red = red.getScaledInstance(100,100, 0);
+        JLabel redPile = new JLabel(new ImageIcon(red));
+
+        //label for num red cards
+        JLabel redLabel = new JLabel("Reds: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("red"));
+        redLabel.setFont(new Font("Calibri",1, 15));
+
+        Image rainbow = driver.getUprightTrans().get(6).getPicture();
+        rainbow = rainbow.getScaledInstance(100,100, 0);
+        JLabel rainbowPile = new JLabel(new ImageIcon(rainbow));
+
+        //label for num rainbow cards
+        JLabel rainbowLabel = new JLabel("Rainbows: " + driver.getPlayers().get(driver.getPlayerTurn()).getNumColor("Rainbow"));
+        rainbowLabel.setFont(new Font("Calibri",1, 15));
+
+        Image destDraw = toolkit.getImage("C:/Users/patri/Documents/eileen/ttrrestored/src/fwdboardandtransport/destination_card_back.jpg");
+        destDraw = destDraw.getScaledInstance(200,100, 0);
+        JLabel destDrawPile = new JLabel(new ImageIcon(destDraw));
+
+        //score panel
+        JPanel scorePanel = new JPanel();
+        scorePanel.setSize(new Dimension(200, 100));
+        scorePanel.setBackground(Color.WHITE);
+        scorePanel.setLayout(new GridLayout(3,1));
+
+        JLabel taxiLabel = new JLabel("Taxis: " + driver.getPlayers().get(driver.getPlayerTurn()).getTaxis());
+        taxiLabel.setFont(new Font("Calibri",1, 15));
+        scorePanel.add(taxiLabel);
+        scorePanel.setBorder(new LineBorder(Color.BLACK));
+
+        JButton seeDestButton = new JButton("See destination cards");
+        scorePanel.add(seeDestButton);
+
+        seeDestButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                String message = "You have these cards:";
+                for(DestinationCard d: driver.getPlayers().get(driver.getPlayerTurn()).getDestHand()){
+                    message = message + " " + d.toString();
+                }
+                JOptionPane.showMessageDialog(frame, message);
+            }
+        });
+
+
+
+        JLabel touristLabel = new JLabel("Tourist Attractions: ");
+        touristLabel.setFont(new Font("Calibri",1, 15));
+        scorePanel.add(touristLabel);
+        scorePanel.setBorder(new LineBorder(Color.BLACK));
+
+
+        transCardHandPanel.add(nameLabel);
+        transCardHandPanel.add(bluePile);
+        transCardHandPanel.add(blankLabel);
+        transCardHandPanel.add(blueLabel);
+        transCardHandPanel.add(grayPile);
+        transCardHandPanel.add(greenPile);
+        transCardHandPanel.add(grayLabel);
+        transCardHandPanel.add(greenLabel);
+        transCardHandPanel.add(orangePile);
+        transCardHandPanel.add(pinkPile);
+        transCardHandPanel.add(orangeLabel);
+        transCardHandPanel.add(pinkLabel);
+
+        transCardHandPanel.add(redPile);
+        transCardHandPanel.add(rainbowPile);
+        transCardHandPanel.add(redLabel);
+        transCardHandPanel.add(rainbowLabel);
+        p.add(transCardHandPanel);
+        p.add(scorePanel);
+        p.add(destDrawPile);
+
+
+        return p;
+    }
+
+
+
+    /*creates panel in center for board*/
+    public  JPanel boardPanel(){
+
+        JPanel p = new JPanel();
+        p.setSize(new Dimension(600,600));
+        p.setBackground(Color.GRAY);
+        JPanel panel = new JPanel();
+        panel.setBorder(new LineBorder(Color.BLACK)); // make it easy to see
+
+        //add the board image to the panel
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image boardImage = toolkit.getImage("C:/Users/patri/Documents/eileen/ttrrestored/src/fwdboardandtransport/game_board.jpg");
+
+        boardImage = boardImage.getScaledInstance(650,700, 0);
+        JLabel picLabel = new JLabel(new ImageIcon(boardImage));
+        panel.add(picLabel);
+        panel.repaint();
+
+        p.add(panel);
+        return p;
+    }
+    /**
+     * This method makes sure the mouse does nothing when the mouse enters the panel.
+     * @param e   the event the mouse triggers
+     */
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    /**
+     * This method makes sure the mouse does nothing when the mouse exits the panel.
+     * @param e   the event the mouse triggers
+     */
+    public void mouseExited(MouseEvent e)
+    {
+    }
+
+    /**
+     * This method makes sure the mouse does nothing when the mouse is pressed down.
+     * @param e   the event the mouse triggers
+     */
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    /**
+     * This method makes sure the mouse does nothing when the mouse button is released.
+     * @param e   the event the mouse triggers
+     */
+    public void mouseReleased(MouseEvent e)
+    {
+    }
+
+    /**
+     * This method makes sure the mouse does nothing when the mouse is moved.
+     * @param e   the event the mouse triggers
+     */
+    public void mouseWheelMoved(MouseWheelEvent e)
+    {
+    }
+
+    /**
+     * This method handles mouse clicking events. This includes clicking "play game" to start the game, and the functionality to interact with the board.
+     * @param e   the event the mouse triggers
+     */
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+
+    /**
+     * Main method to run program
+     * @param args command line arguments.
+     */
+    static public void main(String[] args)
+    {
+        GUITest t = new GUITest();
+        /* Schedule a job for the event-dispatching thread: creating and showing this application's GUI. Unsupported feature in Stride : anonymous class*/
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                t.createAndShowGUI();
+            }
+        });
+    }
+
+}
