e19b706ea7fc452472277afc503e0c7085c8fb86
Greg MacGown <31485651+gmacg1@users.noreply.github.com>
2019-04-27 22:37:47 -0400

    Added boolean for if is tourist attraction 4 score

--- Location.java
+++ Location.java
@@ -4,15 +4,18 @@ public class Location {
     //x coord
     int xCoord;
     int yCoord;
+    boolean attraction;
 
-    public Location(String name, int xCoord, int yCoord){
+    public Location(String name, int xCoord, int yCoord,boolean isTouristAttraction){
         this.name = name;
         this.xCoord = xCoord;
         this.yCoord = yCoord;
-
+        attraction = isTouristAttraction;
     }
 
     public String getName(){
         return name;
     }
+    
+    public boolean isAttraction() { return attraction;}
 }
