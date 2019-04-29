9e6ef4bb46375318d4f45db8b1aeb0f14d893be5
Nevno48 <nevno48@gmail.com>
2019-04-29 09:51:27 -0400

    Add files via upload
    added comments

--- Location.java
+++ Location.java
@@ -1,11 +1,35 @@
+/**
+ * Determines route name, coordinates, and attraction.
+ *
+ * @author Eileen Bohen
+ * @version Spring 2019
+ */
 public class Location {
-    //name
+    /**
+     * Name of the route
+     */
     String name;
-    //x coord
+    /**
+     * X coordinate of route.
+     */
     int xCoord;
+    /**
+     * Y coordinate of route.
+     */
     int yCoord;
+    /**
+     * Whether or not route is attraction.
+     */
     boolean attraction;
 
+    /**
+     * Constructor for Route objects, initializes instance variables.
+     * 
+     * @param name Name of route .
+     * @param xCoord X coordinate of route.
+     * @param yCoord Y coordinate of route.
+     * @param isTouristAttraction Whether or not route is tourist attraction.
+     */
     public Location(String name, int xCoord, int yCoord,boolean isTouristAttraction){
         this.name = name;
         this.xCoord = xCoord;
@@ -13,9 +37,21 @@ public class Location {
         attraction = isTouristAttraction;
     }
 
+    /**
+     * Returns name of Route as String.
+     * 
+     * @return The name of the route.
+     */
     public String getName(){
         return name;
     }
     
-    public boolean isAttraction() { return attraction;}
+    /**
+     * Returns whether or not route is tourist attraction
+     * 
+     * @return True if route is attraction, false otherwise.
+     */
+    public boolean isAttraction() { 
+        return attraction;
+    }
 }
